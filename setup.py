fcfabc802337ac6f1aae5dfc48dcdb6f73a523a4
Marco Pracucci <marco.pracucci@spreaker.com>
2017-10-17 15:41:49 +0200

    Fixed PyPI packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(
   name = 'prometheus-jenkins-exporter',
   packages = ['prometheus-jenkins-exporter'],
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Prometheus exporter for Jenkins',
   author = 'Marco Pracucci',
   author_email = 'marco@pracucci.com',
   url = 'https://github.com/spreaker/prometheus-jenkins-exporter',
-  download_url = 'https://github.com/spreaker/prometheus-jenkins-exporter/archive/0.1.2.tar.gz',
+  download_url = 'https://github.com/spreaker/prometheus-jenkins-exporter/archive/0.1.3.tar.gz',
   keywords = ['prometheus', 'jenkins'],
   classifiers = [],
   python_requires = '>=3',
