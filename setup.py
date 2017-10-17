963654f81d84703ab27981ee702a5f5f8d7abc25
Marco Pracucci <marco.pracucci@spreaker.com>
2017-10-17 15:51:15 +0200

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from setuptools import setup
 setup(
   name = 'prometheus-jenkins-exporter',
   packages = ['prometheus_jenkins_exporter'],
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'Prometheus exporter for Jenkins',
   author = 'Marco Pracucci',
   author_email = 'marco@pracucci.com',
   url = 'https://github.com/spreaker/prometheus-jenkins-exporter',
-  download_url = 'https://github.com/spreaker/prometheus-jenkins-exporter/archive/0.1.4.tar.gz',
+  download_url = 'https://github.com/spreaker/prometheus-jenkins-exporter/archive/0.1.5.tar.gz',
   keywords = ['prometheus', 'jenkins'],
   classifiers = [],
   python_requires = '>=3',
+  install_requires=['httplib2==0.10.3', 'prometheus_client==0.0.21', 'python-json-logger==0.1.5'],
   entry_points={
     'console_scripts': [
         'jenkins-exporter=prometheus_jenkins_exporter.exporter:main',
